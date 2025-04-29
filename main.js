3f2c960ab3ac39dab2cb374f14ff3a5b7a9bfee3
pegozzigabriel <pegozzigabriel@gmail.com>
2025-04-29 12:00:39 +0000

    exemplo 2

--- main.js
+++ main.js
@@ -4,3 +4,18 @@ function mensagem(){
     console.log("Seja bem vindo")
     console.log("Você é Muito Legal!")
 }
+
+   */
+/*exemplo 2
+function somar(numero1, numero2){
+    console.log(numero1 + numero2)
+}
+
+let numero1 = 60
+let numero2 = 23
+
+somar(numero1, numero2)
+somar(30,20)
+somar(19, 289)
+
+*/
